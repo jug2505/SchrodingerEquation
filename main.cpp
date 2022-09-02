6141cf5866afd68a10759225b76c11cd4aafb143
Evgeny Kuvshinov <41694637+jug2505@users.noreply.github.com>
2022-09-02 16:48:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
 #include <vector>
-
-#define M_PI 3.14159265358979323846
-
+#include <complex>
+#include <cmath>
 
 using namespace std;
 
 class Besse {
 public:
+    double lambda = 0.0; // TODO: С типом пока не понятно
+    double A = 0.0; // TODO: С типом пока не понятно
+    double B = 0.0; // TODO: С типом пока не понятно
+    double C = 0.0; // TODO: С типом пока не понятно
+
+    vector<vector<complex<double>>> u; // Решение в точке [t_n][x_m]
+
+    vector<double> x; // Сетка x
+    double h; // Шаг по x
     int M; // Количество разбиений по x
+
+    vector<double> t; // Сетка t
+    double k; // Шаг по t
     int N; // Количество разбиений по t
-    double lambda;
-    double A;
-    double B;
-    double C;
-    
     double tStart = 0.0; // t начала
     double tStop = 5.0; // t конца
-    
-    vector<vector<double>> u; // Решение в точке [t_n][x_m]
-    vector<double> x; // Сетка x
-    vector<double> t; // Сетка t
-    
+
     Besse(int M, int N) {
         this->M = M;
         this->N = N;
-        prepareX(M);
-        prepareT(N);
+
+        prepareX();
+        prepareT();
     }
 
-    void prepareX(int M) {
+    void prepareX() {
         x = vector<double>(M + 2);
-        const double h = 2 * M_PI / (M + 1);
+        h = 2 * M_PI / (M + 1);
         for (int m = 0; m <= M + 1; m++) {
             x[m] = -M_PI + m * h;
         }
     }
-    
-    void prepareT(int N) {
-        t = vector<double>(N + 2);
-        const double k = (tStop - tStart) / N;
-        for (int n = 0; n <= N + 1; n++) {
+
+    void prepareT() {
+        t = vector<double>(N + 1);
+        k = (tStop - tStart) / N;
+        for (int n = 0; n <= N; n++) {
             t[n] = n * k;
         }
     }
