0a72159537910be83809a4d1ce7fed6fe37e6730
jug2505 <jonkuvshin@gmail.com>
2022-11-04 16:13:57 +0300

    refactor: besse

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <complex>
 #include <cmath>
@@ -12,33 +13,42 @@ using namespace Eigen;
 
 class Besse {
 public:
-    double lambda = 1.0; // TODO: С типом пока не понятно
-    double A = 1.0; // TODO: С типом пока не понятно
-    double B = 1.0; // TODO: С типом пока не понятно
-    double C = 2.0; // TODO: С типом пока не понятно
+    function<complex<double>(double)> init_func; // f(x) инициализации
 
     MatrixXcd U; // Решение в точке [t_n][x_m]
+    MatrixXcd U_analytic;
 
     VectorXd x; // Сетка x
-    double h; // Шаг по x
+    double h = 0.0; // Шаг по x
     int M; // Количество разбиений по x
 
     VectorXd t; // Сетка t
-    double k; // Шаг по t
+    double k = 0.0; // Шаг по t
     int N; // Количество разбиений по t
-    double tStart = -1.0; // t начала
-    double tStop = 1.0; // t конца
 
-    Besse(int M, int N) {
+    double lambda;
+    double A, B, C;
+
+    double t_start = -1.0; // t начала
+    double t_stop = 1.0; // t конца
+
+    Besse(int M, int N, double t_start, double t_stop, double lambda, function<complex<double>(double)> init_func, double A, double B, double C) {
         this->M = M;
         this->N = N;
+        this->t_start = t_start;
+        this->t_stop = t_stop;
+        this->lambda = lambda;
+        this->init_func = std::move(init_func);
+        this->A = A;
+        this->B = B;
+        this->C = C;
 
-        prepareX();
-        prepareT();
-        prepareU();
+        prepare_x();
+        prepare_t();
+        prepare_u();
     }
 
-    void prepareX() {
+    void prepare_x() {
         x = VectorXd(M + 1); // TODO: M + 2?
         h = 2 * M_PI / (M + 1);
         for (int m = 0; m <= M; m++) { // TODO: M + 1?
@@ -46,43 +56,44 @@ public:
         }
     }
 
-    void prepareT() {
+    void prepare_t() {
         t = VectorXd(N + 1);
-        k = (tStop - tStart) / N;
+        k = (t_stop - t_start) / N;
         for (int n = 0; n <= N; n++) {
             t[n] = n * k;
         }
     }
 
-    void prepareU() {
+    void prepare_u() {
         U = MatrixXcd(N + 1, M + 1);
         for (int m = 0; m <= M; m++) {
-            U(0, m) = exp(complex<double>(0.0, 1.0) * x(m)); // e^(ix) - начальная функция
+            U(0, m) = init_func(x(m));
+
         }
     }
 
-    void writeRealMatrixToFile(const MatrixXcd& src, const string& pathAndName) {
+    void write_real_matrix_to_file(const MatrixXcd& src, const string& path_and_name) {
         MatrixXd real = src.real();
-        writeMatrixToFile(real, pathAndName);
+        write_matrix_to_file(real, path_and_name);
     }
 
-    void writeImagMatrixToFile(const MatrixXcd& src, const string& pathAndName) {
+    void write_imag_matrix_to_file(const MatrixXcd& src, const string& path_and_name) {
         MatrixXd imag = src.imag();
-        writeMatrixToFile(imag, pathAndName);
+        write_matrix_to_file(imag, path_and_name);
     }
 
-    void writeAbsMatrixToFile(const MatrixXcd& src, const string& pathAndName) {
+    void write_abs_matrix_to_file(const MatrixXcd& src, const string& path_and_name) {
         MatrixXd imag = MatrixXd(src.rows(), src.cols());
         for (int t_idx = 0; t_idx < src.rows(); t_idx++) {
             for (int x_idx = 0; x_idx < src.cols(); x_idx++) {
                imag(t_idx, x_idx) = abs(src(t_idx, x_idx));
             }
         }
-        writeMatrixToFile(imag, pathAndName);
+        write_matrix_to_file(imag, path_and_name);
     }
 
-    void writeMatrixToFile(const MatrixXd& src, const string& pathAndName) {
-        ofstream file(pathAndName, ios::out | ios::trunc);
+    void write_matrix_to_file(const MatrixXd& src, const string& path_and_name) {
+        ofstream file(path_and_name, ios::out | ios::trunc);
         file << "X T Z" << endl;
         if(file) {
             for (int t_idx = 0; t_idx < t.size(); t_idx++) {
@@ -94,11 +105,24 @@ public:
         }
     }
 
+    void analytic_solution() {
+        U_analytic = MatrixXcd(N + 1, M + 1);
+        for (int t_idx = 0; t_idx < t.size(); t_idx++) {
+            for (int x_idx = 0; x_idx < x.size(); x_idx++) {
+                U_analytic(t_idx, x_idx) = A * exp(complex<double>(0.0, 1.0) * (B * x(x_idx) - (A * A * lambda + B * B) * t(t_idx) + C));
+            }
+        }
+    }
+
     void solve() {
         compute();
-        writeRealMatrixToFile(U, "../besse/real.txt");
-        writeImagMatrixToFile(U, "../besse/imag.txt");
-        writeAbsMatrixToFile(U, "../besse/abs.txt");
+        write_real_matrix_to_file(U, "../besse/real.txt");
+        write_imag_matrix_to_file(U, "../besse/imag.txt");
+        write_abs_matrix_to_file(U, "../besse/abs.txt");
+        analytic_solution();
+        write_real_matrix_to_file(U_analytic, "../besse/analytic_real.txt");
+        write_imag_matrix_to_file(U_analytic, "../besse/analytic_imag.txt");
+        write_abs_matrix_to_file(U_analytic, "../besse/analytic_abs.txt");
     }
 
     void compute() {
@@ -165,6 +189,14 @@ public:
 
 int main()
 {
-    Besse besse = Besse(20, 20);
+    Besse besse = Besse(20,
+                        20,
+                        -1.0,
+                        1.0,
+                        1.0,
+                        [](double x_val) -> complex<double> {return exp(complex<double>(0.0, 1.0) * x_val);}, // e^(ix) - начальная функция
+                        1.0,
+                        1.0,
+                        0.0);
     besse.solve();
 }
