4ae407522f52125549f4e2191411445476893d10
jug2505 <jonkuvshin@gmail.com>
2022-11-06 13:13:50 +0300

    besse: bi-soliton

--- main.cpp
+++ main.cpp
@@ -29,10 +29,11 @@ public:
     double lambda;
     double A, B, C;
 
-    double t_start = -1.0; // t начала
+    double t_start = 0.0; // t начала
     double t_stop = 1.0; // t конца
 
-    Besse(int M, int N, double t_start, double t_stop, double lambda, function<complex<double>(double)> init_func, double A, double B, double C) {
+    Besse(int M, int N, double t_start, double t_stop, double lambda,
+          function<complex<double>(double)> init_func, double A, double B, double C) {
         this->M = M;
         this->N = N;
         this->t_start = t_start;
@@ -49,26 +50,40 @@ public:
     }
 
     void prepare_x() {
+        cout << "BESSE: prepare X" << endl;
         x = VectorXd(M + 1); // TODO: M + 2?
         h = 2 * M_PI / (M + 1);
         for (int m = 0; m <= M; m++) { // TODO: M + 1?
             x[m] = -M_PI + m * h;
         }
+        cout << "h: " << h << endl;
     }
 
     void prepare_t() {
+        cout << "BESSE: prepare T" << endl;
         t = VectorXd(N + 1);
         k = (t_stop - t_start) / N;
         for (int n = 0; n <= N; n++) {
             t[n] = n * k;
         }
+        cout << "k: " << k << endl;
     }
 
     void prepare_u() {
+        cout << "BESSE: prepare U" << endl;
         U = MatrixXcd(N + 1, M + 1);
-        for (int m = 0; m <= M; m++) {
-            U(0, m) = init_func(x(m));
-
+//        for (int m = 0; m <= M; m++) {
+//            U(0, m) = init_func(x(m));
+//
+//        }
+        // Bi-soliton
+        for (int x_idx = 0; x_idx < x.size(); x_idx++) {
+            complex<double> t1 = 2.0 * complex<double>(0.0, 1.0);
+            complex<double> _tmp1 = (1.0 + 2.0 * complex<double>(0.0, 1.0) * t(0)) * cosh(x(x_idx)) - x(x_idx) * sinh(x(x_idx));
+            complex<double> _tmp2 = 1.0 + 2.0 * x(x_idx) * x(x_idx) + 8.0 * t(0) * t(0) + cosh(2.0 * x(x_idx));
+            complex<double> _tmp3 = 4.0 * exp(complex<double>(0.0, 1.0) * t(0));
+            complex<double> _tmp4 = _tmp3 * _tmp1 / _tmp2;
+            U(0, x_idx) = _tmp4;
         }
     }
 
@@ -107,11 +122,34 @@ public:
 
     void analytic_solution() {
         U_analytic = MatrixXcd(N + 1, M + 1);
+//        for (int t_idx = 0; t_idx < t.size(); t_idx++) {
+//            for (int x_idx = 0; x_idx < x.size(); x_idx++) {
+//                U_analytic(t_idx, x_idx) = A * exp(complex<double>(0.0, 1.0) * (B * x(x_idx) - (A * A * lambda + B * B) * t(t_idx) + C));
+//            }
+//        }
+        // B-soliton
         for (int t_idx = 0; t_idx < t.size(); t_idx++) {
             for (int x_idx = 0; x_idx < x.size(); x_idx++) {
-                U_analytic(t_idx, x_idx) = A * exp(complex<double>(0.0, 1.0) * (B * x(x_idx) - (A * A * lambda + B * B) * t(t_idx) + C));
+                complex<double> _tmp1 = (1.0 + 2.0 * complex<double>(0.0, 1.0) * t(t_idx)) * cosh(x(x_idx)) - x(x_idx) * sinh(x(x_idx));
+                complex<double> _tmp2 = 1.0 + 2.0 * x(x_idx) * x(x_idx) + 8.0 * t(t_idx) * t(t_idx) + cosh(2.0 * x(x_idx));
+                complex<double> _tmp3 = 4.0 * exp(complex<double>(0.0, 1.0) * t(t_idx));
+                U_analytic(t_idx, x_idx) = _tmp3 * _tmp1 / _tmp2;
             }
         }
+    }
+
+    VectorXd absolute_error() {
+        VectorXd error_vector = VectorXd(U.rows());
+        MatrixXcd error_matrix = U_analytic - U;
+        for (int t_idx = 0; t_idx < t.size(); t_idx++) {
+            double absolute_error = 0.0;
+            for (int x_idx = 0; x_idx < x.size(); x_idx++) {
+                absolute_error += abs(error_matrix(t_idx, x_idx).real());
+                absolute_error += abs(error_matrix(t_idx, x_idx).imag());
+            }
+            error_vector(t_idx) = absolute_error / (2.0 * (double)x.size());
+        }
+        return error_vector;
     }
 
     void solve() {
@@ -129,15 +167,26 @@ public:
         VectorXcd V_minus = VectorXcd(M + 1);
         VectorXcd V_plus = VectorXcd(M + 1);
 
-        V_minus = U.row(0).cwiseAbs2(); // V^-1/2 = |U|^2
+//        V_minus = U.row(0).cwiseAbs2(); // V^-1/2 = |U|^2
+        for (int x_idx = 0; x_idx < U.cols(); x_idx++) {
+            V_minus(x_idx) = abs(U(0, x_idx)) * abs(U(0, x_idx));
+        }
 
-        complex<double> r = k / (2 * h * h);
+        double r = k / (2.0 * h * h);
+        cout << "r: " << r << endl;
+
 
         for (int n = 0; n < N; n++) { // 0 -> N-1 / Считаем U от 1 до N
+//            cout << "BESSE: STEP " << n << "/" << N-1 << endl;
             // Вычисление V_plus
             VectorXcd _tmp_vector1 = -V_minus;
-            VectorXcd _tmp_vector2 = 2.0 * U.row(n).cwiseAbs2();
-            V_plus = _tmp_vector1 + _tmp_vector2;
+            VectorXcd _tmp_vector3 = VectorXcd(U.cols());
+            for (int x_idx = 0; x_idx < U.cols(); x_idx++) {
+                _tmp_vector3(x_idx) = 2.0 * abs(U(n, x_idx)) * abs(U(n, x_idx));
+            }
+            //VectorXcd _tmp_vector2 = 2.0 * U.row(n).cwiseAbs2();
+            VectorXcd _tmp_vector2;
+            V_plus = _tmp_vector1 + _tmp_vector3;
 
             MatrixXcd A_plus = MatrixXcd(M + 1, M + 1);
             // Первая строка
@@ -187,16 +236,19 @@ public:
     }
 };
 
+
 int main()
 {
-    Besse besse = Besse(20,
-                        20,
-                        -1.0,
+    Besse* besse = new Besse(100,
+                        1000,
+                        0.0,
                         1.0,
-                        1.0,
+                        -2.0,
                         [](double x_val) -> complex<double> {return exp(complex<double>(0.0, 1.0) * x_val);}, // e^(ix) - начальная функция
                         1.0,
                         1.0,
                         0.0);
-    besse.solve();
+    besse->solve();
+    cout << besse->absolute_error() << endl;
+
 }
