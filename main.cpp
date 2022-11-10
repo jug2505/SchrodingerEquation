188b933730ecce43de226667357b8f83789ab5d1
Evgeny Kuvshinov <kuvshinov@axiomatika.ru>
2022-11-10 15:55:09 +0300

    feat: working besse

--- main.cpp
+++ main.cpp
@@ -13,37 +13,32 @@ using namespace Eigen;
 
 class Besse {
 public:
-    function<complex<double>(double)> init_func; // f(x) инициализации
+    function<complex<double>(double)> init_func = nullptr; // f(x) инициализации
 
     MatrixXcd U; // Решение в точке [t_n][x_m]
     MatrixXcd U_analytic;
+    MatrixXd error_matrix;
 
     VectorXd x; // Сетка x
-    double h = 0.0; // Шаг по x
+    double x_start = -M_PI; // x начала
+    double x_stop = M_PI; // x конца
     int M; // Количество разбиений по x
+    double h = 0.0; // Шаг по x
 
     VectorXd t; // Сетка t
-    double k = 0.0; // Шаг по t
-    int N; // Количество разбиений по t
-
-    double lambda;
-    double A, B, C;
-
     double t_start = 0.0; // t начала
     double t_stop = 1.0; // t конца
+    int N; // Количество разбиений по t
+    double k = 0.0; // Шаг по t
 
-    Besse(int M, int N, double t_start, double t_stop, double lambda,
-          function<complex<double>(double)> init_func, double A, double B, double C) {
-        this->M = M;
-        this->N = N;
-        this->t_start = t_start;
-        this->t_stop = t_stop;
-        this->lambda = lambda;
-        this->init_func = std::move(init_func);
-        this->A = A;
-        this->B = B;
-        this->C = C;
+    double lambda;
+    double A, B, C; // Коэффициенты аналитического решения для f(x) = exp(ix)
+    bool is_bi_soliton = false;
+    bool is_exp_ix = false;
 
+    bool has_analytic = false;
+
+    void init() {
         prepare_x();
         prepare_t();
         prepare_u();
@@ -51,39 +46,50 @@ public:
 
     void prepare_x() {
         cout << "BESSE: prepare X" << endl;
-        x = VectorXd(M + 1); // TODO: M + 2?
-        h = 2 * M_PI / (M + 1);
-        for (int m = 0; m <= M; m++) { // TODO: M + 1?
-            x[m] = -M_PI + m * h;
-        }
+        x = VectorXd::Zero(M + 1); // TODO: M + 2?
+        h = (x_stop - x_start) / M;
         cout << "h: " << h << endl;
+        for (int m = 0; m <= M; m++) {
+            x[m] = x_start + m * h;
+        }
     }
 
     void prepare_t() {
         cout << "BESSE: prepare T" << endl;
-        t = VectorXd(N + 1);
+        t = VectorXd::Zero(N + 1);
         k = (t_stop - t_start) / N;
-        for (int n = 0; n <= N; n++) {
-            t[n] = n * k;
-        }
         cout << "k: " << k << endl;
+        for (int n = 0; n <= N; n++) {
+            t[n] = t_start + n * k;
+        }
     }
 
     void prepare_u() {
-        cout << "BESSE: prepare U" << endl;
-        U = MatrixXcd(N + 1, M + 1);
-//        for (int m = 0; m <= M; m++) {
-//            U(0, m) = init_func(x(m));
-//
-//        }
-        // Bi-soliton
+        if (is_bi_soliton) {
+            prepare_u_bi_soluton();
+        }
+        if (init_func) {
+            prepare_u_with_init_func();
+        }
+    }
+
+    void prepare_u_bi_soluton() {
+        cout << "BESSE: prepare U Bi-Soliton" << endl;
+        U = MatrixXcd::Zero(N + 1, M + 1);
         for (int x_idx = 0; x_idx < x.size(); x_idx++) {
-            complex<double> t1 = 2.0 * complex<double>(0.0, 1.0);
             complex<double> _tmp1 = (1.0 + 2.0 * complex<double>(0.0, 1.0) * t(0)) * cosh(x(x_idx)) - x(x_idx) * sinh(x(x_idx));
             complex<double> _tmp2 = 1.0 + 2.0 * x(x_idx) * x(x_idx) + 8.0 * t(0) * t(0) + cosh(2.0 * x(x_idx));
             complex<double> _tmp3 = 4.0 * exp(complex<double>(0.0, 1.0) * t(0));
             complex<double> _tmp4 = _tmp3 * _tmp1 / _tmp2;
             U(0, x_idx) = _tmp4;
+        }
+    }
+
+    void prepare_u_with_init_func() {
+        cout << "BESSE: prepare U with init function" << endl;
+        U = MatrixXcd::Zero(N + 1, M + 1);
+        for (int m = 0; m <= M; m++) {
+            U(0, m) = init_func(x(m));
         }
     }
 
@@ -98,7 +104,7 @@ public:
     }
 
     void write_abs_matrix_to_file(const MatrixXcd& src, const string& path_and_name) {
-        MatrixXd imag = MatrixXd(src.rows(), src.cols());
+        MatrixXd imag = MatrixXd::Zero(src.rows(), src.cols());
         for (int t_idx = 0; t_idx < src.rows(); t_idx++) {
             for (int x_idx = 0; x_idx < src.cols(); x_idx++) {
                imag(t_idx, x_idx) = abs(src(t_idx, x_idx));
@@ -108,9 +114,10 @@ public:
     }
 
     void write_matrix_to_file(const MatrixXd& src, const string& path_and_name) {
+        cout << "BESSE: write matrix " << path_and_name << endl;
         ofstream file(path_and_name, ios::out | ios::trunc);
-        file << "X T Z" << endl;
         if(file) {
+            file << "X T Z" << endl;
             for (int t_idx = 0; t_idx < t.size(); t_idx++) {
                 for (int x_idx = 0; x_idx < x.size(); x_idx++) {
                     file << x(x_idx) << " " << t(t_idx) << " " << src(t_idx, x_idx) << endl;
@@ -121,13 +128,29 @@ public:
     }
 
     void analytic_solution() {
-        U_analytic = MatrixXcd(N + 1, M + 1);
-//        for (int t_idx = 0; t_idx < t.size(); t_idx++) {
-//            for (int x_idx = 0; x_idx < x.size(); x_idx++) {
-//                U_analytic(t_idx, x_idx) = A * exp(complex<double>(0.0, 1.0) * (B * x(x_idx) - (A * A * lambda + B * B) * t(t_idx) + C));
-//            }
-//        }
-        // B-soliton
+        if (is_bi_soliton) {
+            analytic_solution_bi_soliton();
+            has_analytic = true;
+        }
+        if (is_exp_ix) {
+            analytic_solution_exp_ix();
+            has_analytic = true;
+        }
+    }
+
+    void analytic_solution_exp_ix() {
+        cout << "BESSE: analytic solution exp(ix)" << endl;
+        U_analytic = MatrixXcd::Zero(N + 1, M + 1);
+        for (int t_idx = 0; t_idx < t.size(); t_idx++) {
+            for (int x_idx = 0; x_idx < x.size(); x_idx++) {
+                U_analytic(t_idx, x_idx) = A * exp(complex<double>(0.0, 1.0) * (B * x(x_idx) - (A * A * lambda + B * B) * t(t_idx) + C));
+            }
+        }
+    }
+
+    void analytic_solution_bi_soliton() {
+        cout << "BESSE: analytic solution bi-soliton" << endl;
+        U_analytic = MatrixXcd::Zero(N + 1, M + 1);
         for (int t_idx = 0; t_idx < t.size(); t_idx++) {
             for (int x_idx = 0; x_idx < x.size(); x_idx++) {
                 complex<double> _tmp1 = (1.0 + 2.0 * complex<double>(0.0, 1.0) * t(t_idx)) * cosh(x(x_idx)) - x(x_idx) * sinh(x(x_idx));
@@ -138,36 +161,39 @@ public:
         }
     }
 
-    VectorXd absolute_error() {
-        VectorXd error_vector = VectorXd(U.rows());
-        MatrixXcd error_matrix = U_analytic - U;
+    void compute_absolute_error() {
+        cout << "BESSE: compute absolute error" << endl;
+        error_matrix = MatrixXd::Zero(N + 1, M + 1);
         for (int t_idx = 0; t_idx < t.size(); t_idx++) {
-            double absolute_error = 0.0;
             for (int x_idx = 0; x_idx < x.size(); x_idx++) {
-                absolute_error += abs(error_matrix(t_idx, x_idx).real());
-                absolute_error += abs(error_matrix(t_idx, x_idx).imag());
+                error_matrix(t_idx, x_idx) = abs(U_analytic(t_idx, x_idx) - U(t_idx, x_idx));
             }
-            error_vector(t_idx) = absolute_error / (2.0 * (double)x.size());
         }
-        return error_vector;
     }
 
-    void solve() {
+    void solve(const string& folder) {
+        cout << "BESSE: in solve" << endl;
+        init();
         compute();
-        write_real_matrix_to_file(U, "../besse/real.txt");
-        write_imag_matrix_to_file(U, "../besse/imag.txt");
-        write_abs_matrix_to_file(U, "../besse/abs.txt");
+        write_real_matrix_to_file(U, folder + "/real.txt");
+        write_imag_matrix_to_file(U, folder + "/imag.txt");
+        write_abs_matrix_to_file(U, folder + "/abs.txt");
         analytic_solution();
-        write_real_matrix_to_file(U_analytic, "../besse/analytic_real.txt");
-        write_imag_matrix_to_file(U_analytic, "../besse/analytic_imag.txt");
-        write_abs_matrix_to_file(U_analytic, "../besse/analytic_abs.txt");
+        if (has_analytic) {
+            write_real_matrix_to_file(U_analytic, folder + "/real_analytic.txt");
+            write_imag_matrix_to_file(U_analytic, folder + "/imag_analytic.txt");
+            write_abs_matrix_to_file(U_analytic, folder + "/abs_analytic.txt");
+            compute_absolute_error();
+            write_matrix_to_file(error_matrix, folder + "/error_matrix.txt");
+        }
     }
 
     void compute() {
-        VectorXcd V_minus = VectorXcd(M + 1);
-        VectorXcd V_plus = VectorXcd(M + 1);
+        cout << "BESSE: in compute" << endl;
+        VectorXcd V_minus = VectorXcd::Zero(M + 1);
+        VectorXcd V_plus = VectorXcd::Zero(M + 1);
 
-//        V_minus = U.row(0).cwiseAbs2(); // V^-1/2 = |U|^2
+        // V^-1/2 = |U|^2
         for (int x_idx = 0; x_idx < U.cols(); x_idx++) {
             V_minus(x_idx) = abs(U(0, x_idx)) * abs(U(0, x_idx));
         }
@@ -175,20 +201,20 @@ public:
         double r = k / (2.0 * h * h);
         cout << "r: " << r << endl;
 
-
         for (int n = 0; n < N; n++) { // 0 -> N-1 / Считаем U от 1 до N
-//            cout << "BESSE: STEP " << n << "/" << N-1 << endl;
+            if (n % 10 == 0) {
+                cout << "BESSE: STEP " << n << "/" << N - 1 << endl;
+            }
+
             // Вычисление V_plus
             VectorXcd _tmp_vector1 = -V_minus;
-            VectorXcd _tmp_vector3 = VectorXcd(U.cols());
+            VectorXcd _tmp_vector2 = VectorXcd::Zero(U.cols());
             for (int x_idx = 0; x_idx < U.cols(); x_idx++) {
-                _tmp_vector3(x_idx) = 2.0 * abs(U(n, x_idx)) * abs(U(n, x_idx));
+                _tmp_vector2(x_idx) = 2.0 * abs(U(n, x_idx)) * abs(U(n, x_idx));
             }
-            //VectorXcd _tmp_vector2 = 2.0 * U.row(n).cwiseAbs2();
-            VectorXcd _tmp_vector2;
-            V_plus = _tmp_vector1 + _tmp_vector3;
+            V_plus = _tmp_vector1 + _tmp_vector2;
 
-            MatrixXcd A_plus = MatrixXcd(M + 1, M + 1);
+            MatrixXcd A_plus = MatrixXcd::Zero(M + 1, M + 1);
             // Первая строка
             A_plus(0, 0) = complex<double>(0.0, 1.0) / r - 2.0 - lambda * h * h * V_plus(0);
             A_plus(0, 1) = 1.0;
@@ -206,7 +232,7 @@ public:
             }
             A_plus = r * A_plus;
 
-            MatrixXcd A_minus = MatrixXcd(M + 1, M + 1);
+            MatrixXcd A_minus = MatrixXcd::Zero(M + 1, M + 1);
             // Первая строка
             A_minus(0, 0) = -complex<double>(0.0, 1.0) / r - 2.0 - lambda * h * h * V_plus(0);
             A_minus(0, 1) = 1.0;
@@ -236,19 +262,93 @@ public:
     }
 };
 
+class BesseHelper {
+public:
+    static void compute_bi_soliton_400_400() {
+        Besse* besse = new Besse();
+        besse->is_bi_soliton = true;
+        besse->M = 400;
+        besse->N = 400;
+        besse->t_start = -5.0;
+        besse->t_stop = 5.0;
+        besse->x_start = -3.0 * M_PI;
+        besse->x_stop = 3.0 * M_PI;
+        besse->lambda = -2;
 
-int main()
-{
-    Besse* besse = new Besse(100,
-                        1000,
-                        0.0,
-                        1.0,
-                        -2.0,
-                        [](double x_val) -> complex<double> {return exp(complex<double>(0.0, 1.0) * x_val);}, // e^(ix) - начальная функция
-                        1.0,
-                        1.0,
-                        0.0);
-    besse->solve();
-    cout << besse->absolute_error() << endl;
+        besse->init();
+        besse->solve("../data/bi_soliton_400_400");
+    }
 
+    static void compute_bi_soliton_2000_2000() {
+        Besse* besse = new Besse();
+        besse->is_bi_soliton = true;
+        besse->M = 2000;
+        besse->N = 2000;
+        besse->t_start = -5.0;
+        besse->t_stop = 5.0;
+        besse->x_start = -4.0 * M_PI;
+        besse->x_stop = 4.0 * M_PI;
+        besse->lambda = -2;
+
+        besse->init();
+        besse->solve("../data/bi_soliton_2000_2000");
+    }
+
+    static void compute_exp_ix_400_400() {
+        Besse* besse = new Besse();
+        besse->is_exp_ix = true;
+        besse->M = 400;
+        besse->N = 400;
+        besse->t_start = 0.0;
+        besse->t_stop = 5.0;
+        besse->x_start = -M_PI;
+        besse->x_stop = M_PI;
+        besse->lambda = 1;
+        besse->A = 1;
+        besse->B = 1;
+        besse->C = 0;
+        besse->init_func = [](double x_val) -> complex<double> {return exp(complex<double>(0.0, 1.0) * x_val);};
+
+        besse->init();
+        besse->solve("../data/exp_ix_400_400");
+    }
+
+    static void compute_sin_x_500_1000() {
+        Besse* besse = new Besse();
+        besse->M = 500;
+        besse->N = 1000;
+        besse->t_start = 0.0;
+        besse->t_stop = 5.0;
+        besse->x_start = -M_PI;
+        besse->x_stop = M_PI;
+        besse->lambda = -2;
+        besse->init_func = [](double x_val) -> complex<double> {return {sin(x_val), 0.0};};
+
+        besse->init();
+        besse->solve("../data/sin_x_500_1000");
+    }
+
+    static void compute_exp_ix_400_400_lambda_minus2() {
+        Besse* besse = new Besse();
+        besse->is_exp_ix = true;
+        besse->M = 400;
+        besse->N = 400;
+        besse->t_start = 0.0;
+        besse->t_stop = 5.0;
+        besse->x_start = -M_PI;
+        besse->x_stop = M_PI;
+        besse->lambda = -2;
+        besse->A = 1;
+        besse->B = 1;
+        besse->C = 0;
+        besse->init_func = [](double x_val) -> complex<double> {return exp(complex<double>(0.0, 1.0) * x_val);};
+
+        besse->init();
+        besse->solve("../data/exp_ix_400_400_lambda_minus2");
+    }
+};
+
+
+int main(){
+    BesseHelper::compute_exp_ix_400_400();
 }
