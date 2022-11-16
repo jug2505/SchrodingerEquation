c300804c76aea8403ddfc37351138a34bb1bd4ae
Evgeny <eak@WS-KUVSHINOV.localdomain>
2022-11-16 15:57:01 +0300

    feat: wsl2

--- main.cpp
+++ main.cpp
@@ -341,9 +341,10 @@ public:
 
 
 int main(){
+    Eigen::setNbThreads(6);
     auto begin = chrono::steady_clock::now();
 
-    BesseHelper::compute_bi_soliton_1000_1000();
+    BesseHelper::compute_bi_soliton_500_500();
 
     auto end = chrono::steady_clock::now();
     auto elapsed_m = std::chrono::duration_cast<chrono::minutes>(end - begin);
