7f4775220157bd7ce3ffd30783303f63f509ea1b
Evgeny <kuvshinov@axiomatika.ru>
2023-02-03 10:36:35 +0300

    feat: E0=0.1 and E0=1

--- main.cpp
+++ main.cpp
@@ -440,6 +440,30 @@ public:
         besse->E0 = 0.0;
         besse->solve("../data/nush_analogue_1000_E0_0");
     }
+
+    static void compute_nush_analogue_1000_E0_1() {
+        unique_ptr<Besse> besse(new Besse);
+        besse->M = 1000;
+        besse->N = 1000;
+        besse->t_start = 0.0;
+        besse->t_stop = 30.0;
+        besse->x_start = -15.0;
+        besse->x_stop = 15.0;
+        besse->E0 = 1.0;
+        besse->solve("../data/nush_analogue_1000_E0_1");
+    }
+
+    static void compute_nush_analogue_1000_E0_01() {
+        unique_ptr<Besse> besse(new Besse);
+        besse->M = 1000;
+        besse->N = 1000;
+        besse->t_start = 0.0;
+        besse->t_stop = 30.0;
+        besse->x_start = -15.0;
+        besse->x_stop = 15.0;
+        besse->E0 = 0.1;
+        besse->solve("../data/nush_analogue_1000_E0_01");
+    }
 };
 
 
@@ -447,7 +471,8 @@ int main(){
     Eigen::setNbThreads(6);
     auto begin = chrono::steady_clock::now();
 
-    BesseHelper::compute_nush_analogue_1000_E0_0();
+    BesseHelper::compute_nush_analogue_1000_E0_1();
+    BesseHelper::compute_nush_analogue_1000_E0_01();
 
     auto end = chrono::steady_clock::now();
     auto elapsed_m = std::chrono::duration_cast<chrono::minutes>(end - begin);
